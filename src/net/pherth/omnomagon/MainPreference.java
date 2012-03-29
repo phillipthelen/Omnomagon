477661af1d129a2aa5d456eed8f06eeb9b8eb248
Phillip Thelen <viirus@pherth.net>
2012-03-29 22:39:13 +0200

    fix for older androids, who fucked up the background

--- src/net/pherth/omnomagon/MainPreference.java
+++ src/net/pherth/omnomagon/MainPreference.java
@@ -36,75 +36,76 @@ import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
 public class MainPreference extends SherlockPreferenceActivity implements OnSharedPreferenceChangeListener {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                addPreferencesFromResource(R.xml.mainpreferences);
-                ActionBar actionBar = getSupportActionBar();
-                actionBar.setDisplayShowTitleEnabled(true);
-                actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.action_bar));
-                actionBar.setDisplayShowHomeEnabled(false);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.mainpreferences);
 
-                CharSequence entry;
-                ListPreference cityPref = (ListPreference) findPreference("cityPreference");
-                entry = cityPref.getEntry();
-        		cityPref.setSummary(entry);
+		getListView().setCacheColorHint(0x00000000);
 
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayShowTitleEnabled(true);
+		actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.action_bar));
+		actionBar.setDisplayShowHomeEnabled(false);
 
-                ListPreference mensaPref = (ListPreference) findPreference("mensaPreference");
-                if (cityPref.getValue() != null) {
-        			System.out.println("cityPref " + cityPref.getValue());
-        			System.out.println("mensaPref " + mensaPref);
-                 	mensaPref.setEntries(getResources().getIdentifier(cityPref.getValue(), "array", "net.pherth.omnomagon"));
-                 	mensaPref.setEntryValues(getResources().getIdentifier(cityPref.getValue()+"Values", "array", "net.pherth.omnomagon"));
-                } else {
-                 	mensaPref.setEntries(R.array.beList);
-                 	mensaPref.setEntryValues(R.array.beListValues);
-                }
-                entry = mensaPref.getEntry();
-        		mensaPref.setSummary(entry);
+		CharSequence entry;
+		ListPreference cityPref = (ListPreference) findPreference("cityPreference");
+		entry = cityPref.getEntry();
+		cityPref.setSummary(entry);
 
 
-                ListPreference pricePref = (ListPreference) findPreference("priceCategory");
-                pricePref.setOnPreferenceChangeListener(setListListener());
-                entry = pricePref.getEntry();
-        		pricePref.setSummary(entry);
+		ListPreference mensaPref = (ListPreference) findPreference("mensaPreference");
+		if (cityPref.getValue() != null) {
+			System.out.println("cityPref " + cityPref.getValue());
+			System.out.println("mensaPref " + mensaPref);
+			mensaPref.setEntries(getResources().getIdentifier(cityPref.getValue(), "array", "net.pherth.omnomagon"));
+			mensaPref.setEntryValues(getResources().getIdentifier(cityPref.getValue()+"Values", "array", "net.pherth.omnomagon"));
+		} else {
+			mensaPref.setEntries(R.array.beList);
+			mensaPref.setEntryValues(R.array.beListValues);
+		}
+		entry = mensaPref.getEntry();
+		mensaPref.setSummary(entry);
 
-        		ListPreference testPref = (ListPreference) findPreference("priceCategory");
-        }
+
+		ListPreference pricePref = (ListPreference) findPreference("priceCategory");
+		pricePref.setOnPreferenceChangeListener(setListListener());
+		entry = pricePref.getEntry();
+		pricePref.setSummary(entry);
+
+		ListPreference testPref = (ListPreference) findPreference("priceCategory");
+	}
 
 
-        private OnPreferenceChangeListener setListListener(){
+	private OnPreferenceChangeListener setListListener(){
+		OnPreferenceChangeListener listener = new OnPreferenceChangeListener() {
+			@Override
+			public boolean onPreferenceChange(Preference arg0, Object arg1) {
+				int index = ((ListPreference) arg0).findIndexOfValue(arg1.toString());
+				CharSequence summary = ((ListPreference) arg0).getEntries()[index];
+				arg0.setSummary(summary);
+				return true;
+			}
+		};
+		return listener;
+	}
 
-        	OnPreferenceChangeListener listener = new OnPreferenceChangeListener() {
-				@Override
-				public boolean onPreferenceChange(
-					Preference arg0, Object arg1) {
-						int index = ((ListPreference) arg0).findIndexOfValue(arg1.toString());
-						CharSequence summary = ((ListPreference) arg0).getEntries()[index];
-                		arg0.setSummary(summary);
-                		return true;
-					}
-        	};
-        	return listener;
-        }
-
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        	if (key.equals("cityPreference")) {
-        		ListPreference mensaPref = (ListPreference) findPreference("mensaPreference");
-        		ListPreference cityPref = (ListPreference) findPreference("cityPreference");
-        		if (cityPref.getValue() != null) {
-        			System.out.println("cityPref " + cityPref.getValue());
-        			System.out.println("mensaPref " + mensaPref);
-                 	mensaPref.setEntries(getResources().getIdentifier(cityPref.getValue(), "array", "net.pherth.omnomagon"));
-                 	mensaPref.setEntryValues(getResources().getIdentifier(cityPref.getValue()+"Values", "array", "net.pherth.omnomagon"));
-                } else {
-                 	mensaPref.setEntries(R.array.beList);
-                 	mensaPref.setEntryValues(R.array.beListValues);
-                }
-        		mensaPref.setValueIndex(0);
-        		CharSequence entry = mensaPref.getEntry();
-        		mensaPref.setSummary(entry);
-        	}
-        }
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+		if (key.equals("cityPreference")) {
+			ListPreference mensaPref = (ListPreference) findPreference("mensaPreference");
+			ListPreference cityPref = (ListPreference) findPreference("cityPreference");
+			if (cityPref.getValue() != null) {
+				System.out.println("cityPref " + cityPref.getValue());
+				System.out.println("mensaPref " + mensaPref);
+				mensaPref.setEntries(getResources().getIdentifier(cityPref.getValue(), "array", "net.pherth.omnomagon"));
+				mensaPref.setEntryValues(getResources().getIdentifier(cityPref.getValue()+"Values", "array", "net.pherth.omnomagon"));
+			} else {
+				mensaPref.setEntries(R.array.beList);
+				mensaPref.setEntryValues(R.array.beListValues);
+			}
+			mensaPref.setValueIndex(0);
+			CharSequence entry = mensaPref.getEntry();
+			mensaPref.setSummary(entry);
+		}
+	}
 }
