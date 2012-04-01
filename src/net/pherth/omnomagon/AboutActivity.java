eb29dba569ffbffecb629640cb7ea0b61dc7bdb6
Phillip Thelen <viirus@pherth.net>
2012-04-01 02:39:41 +0200

    nicer preferences with about section

---
+++ src/net/pherth/omnomagon/AboutActivity.java
@@ -0,0 +1,11 @@
+package net.pherth.omnomagon;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: viirus
+ * Date: 01.04.12
+ * Time: 00:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class AboutActivity {
+}
